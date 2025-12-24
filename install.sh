69acb25a26e5c29d18e1813a5c128640cff79b82
Noufal <noufalkakdlr@gmail.com>
2025-12-24 22:42:42 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ echo "Setting up dotfiles..."
 
 stow kitty mpv
 
+if [ -f "gnome-settings.ini" ]; then
+  echo "Loading GNOME settings..."
+  dconf load / <gnome-settings.ini
+else
+  echo "Warning: gnome-settings.ini not found, skipping..."
+fi
+
 echo "Done! All configurations are linked."
