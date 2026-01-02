23514b803e18b89fccd615d13029d1dc4f9509f5
Noufal <noufalkakdlr@gmail.com>
2026-01-02 19:03:11 +0530

    Auto-backup: 2026-01-02 19:03:11

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -6,11 +6,17 @@ return {
     opts = {
       colors = {
         bg = "#161719",
+        split = "#2b303a",
       },
       highlights = {
         Normal = { bg = "#161719" },
-        NormalFloat = { bg = "#161719" },
+        NormalFloat = { bg = "#18191c" },
         NormalNC = { bg = "#161719" },
+
+        WinSeparator = { fg = "#2b303a", bg = "#161719" },
+        NeoTreeWinSeparator = { fg = "#2b303a", bg = "#161719" },
+        VertSplit = { fg = "#2b303a", bg = "#161719" },
+        SnacksPickerBorder = { fg = "#2b303a", bg = "#161719" },
       },
     },
   },
